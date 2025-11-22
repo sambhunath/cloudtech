cc1485a0fd4b08d11364d4696ba96a01db8c1137
Sambhu <sambhunath.samanta@teradata.com>
2025-11-22 15:17:21 +0530

    Initial commit

---
+++ src/org/cloudtech/WelcomeCloudtEch.java
@@ -0,0 +1,11 @@
+package org.cloudtech;
+
+public class WelcomeCloudtEch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to cloud Tech demo");
+	}
+
+}

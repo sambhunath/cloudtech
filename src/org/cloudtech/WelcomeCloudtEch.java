921663bf1d36d29f04b68b65586938c7773b9846
Sambhu <sambhunath.samanta@teradata.com>
2025-11-22 18:05:51 +0530

    clean

--- src/org/cloudtech/WelcomeCloudtEch.java
+++ src/org/cloudtech/WelcomeCloudtEch.java
@@ -3,8 +3,7 @@ package org.cloudtech;
 public class WelcomeCloudtEch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("#####");
 		System.out.println("Welcome to cloud Tech demo");
 	}
 

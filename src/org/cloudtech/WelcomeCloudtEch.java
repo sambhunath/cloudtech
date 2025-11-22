eb620f259228792cbffdd37f7fa15eabeeb248e8
Sambhu <sambhunath.samanta@teradata.com>
2025-11-22 18:08:44 +0530

    clean

--- src/org/cloudtech/WelcomeCloudtEch.java
+++ src/org/cloudtech/WelcomeCloudtEch.java
@@ -3,7 +3,7 @@ package org.cloudtech;
 public class WelcomeCloudtEch {
 
 	public static void main(String[] args) {
-		System.out.println("#####");
+		System.out.println("####");
 		System.out.println("Welcome to cloud Tech demo");
 	}
 
